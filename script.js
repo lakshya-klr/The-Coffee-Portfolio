0dfa73edf6b8da251941d7afd5d3785fcba986e6
lakshya kalra <106165506+lakshya-klr@users.noreply.github.com>
2024-02-17 15:06:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    setTimeout(function () {
+      var preloader = document.getElementById("preloader");
+      var content = document.getElementById("content");
+  
+      preloader.style.display = "none";
+      content.style.opacity = "1";
+    }, 3000);
+  });
+  
+
