2ce847cd987624b8ba25ab271d2b582d7eeff977
lakshya kalra <106165506+lakshya-klr@users.noreply.github.com>
2024-11-15 21:36:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+// const scroll = new LocomotiveScroll();
+
+const navsel=document.getElementById("nav");
+function circlefollowup(){
+  navsel.addEventListener("mousemove",function(dets){
+    const mover = document.getElementById("mover");
+    
+    // Apply translation for both X and Y
+    mover.style.transform = `translate(${dets.clientX-450}px`;
+    mover.style.opacity="1";
+      
+
+  })}
+  function mouseleft(){
+    mover.style.opacity="0";
+  }
+  
+
 document.addEventListener("DOMContentLoaded", function () {
 
     setTimeout(function () {
